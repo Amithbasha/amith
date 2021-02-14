ff5d16b7296bc89df8cf88b85cd9f72179513ae9
Amith Basha <amithbasha01@gmail.com>
2021-02-14 22:17:15 +0530

    Init usage

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -9,14 +9,18 @@ public class ArrayListExample {
 		List.set(0,"green");
 		ArrayList<String> ListDuplicate = (ArrayList<String>) List.clone();
 		List.addAll(ListDuplicate);
-		//int ListSize = List.size(); for get the no.of items in the arrayList
-		//int pos = List.indexOf("black"); for get the position of the element
+		int ListSize = List.size(); /** For get the no.of items in the arrayList */
+		int pos = List.indexOf("black"); /** for get the position of the element */
 		
 		System.out.println(List);
-		//Collections.sort(List); for sorting in asending order
-		//Collections.sort(List,Collections.reverseOrder()); // for sorting in descending order
+		/** for sorting in asending order */
+		Collections.sort(List);
 		System.out.println(List);
-		//System.out.println(List.get(2)); for get the element by its position
+		/** for sorting in descending order */
+		Collections.sort(List,Collections.reverseOrder());
+		System.out.println(List);
+		/** for get the element by its position */
+		System.out.println(List.get(2));
 		System.out.println(ListDuplicate);
 		
 	}
